a560d3ab53eb2dab67032601d440cc778aac2e5d
Thays <thayscasado@hotmail.com>
2021-04-28 22:20:06 -0700

    component css and variables

--- 3.Section/react-complete-guide/src/components/ExpenseItem.js
+++ 3.Section/react-complete-guide/src/components/ExpenseItem.js
@@ -1,7 +1,21 @@
- function ExpenseItem(){
-   return <h2>Expenses Item!</h2>;
- }
+import './ExpenseItem.css'
 
+function ExpenseItem() {
+  // variables
+  const expenseDate= new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
 
- // to use the component outside of the file we need to export it
- export default ExpenseItem;
+  return (
+    <div className="expense-item">
+      <div >{expenseDate.toISOString}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>      
+    </div>
+  );
+}
+
+// to use the component outside of the file we need to export it
+export default ExpenseItem;
