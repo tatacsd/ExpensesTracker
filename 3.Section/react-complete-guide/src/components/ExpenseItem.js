02bd14edfaa1e3596dfd84acf4490d76042a94e5
Thays <thayscasado@hotmail.com>
2021-04-29 18:38:33 -0700

    using props to add the values to a component

--- 3.Section/react-complete-guide/src/components/ExpenseItem.js
+++ 3.Section/react-complete-guide/src/components/ExpenseItem.js
@@ -1,17 +1,19 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-  // variables
-  const expenseDate= new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', {month: 'long'});
+  const year = props.date.toLocaleString('en-US', {year: '2-digit'}); // {year: 'numeric'}
+  const day = props.date.getFullYear(); // toLocaleString('en-US', {day: 'numeric'});
   return (
     <div className="expense-item">
-      <div >{expenseDate.toISOString}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>      
     </div>
   );
