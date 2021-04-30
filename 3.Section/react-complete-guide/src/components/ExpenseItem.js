80f9139183337140e1628f39c318234bc3ef74f6
Thays <thayscasado@hotmail.com>
2021-04-29 19:15:40 -0700

    splitting the components into smaller components

--- 3.Section/react-complete-guide/src/components/ExpenseItem.js
+++ 3.Section/react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const year = props.date.toLocaleString('en-US', {year: '2-digit'}); // {year: 'numeric'}
-  const day = props.date.getFullYear(); // toLocaleString('en-US', {day: 'numeric'});
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
