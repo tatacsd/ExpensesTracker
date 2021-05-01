e80cb91d07938441dcefda8590d21816eaf37941
Thays <thayscasado@hotmail.com>
2021-04-30 23:58:38 -0700

    updated expanses.js

--- 3.Section/react-complete-guide/src/components/Expenses.js
+++ 3.Section/react-complete-guide/src/components/Expenses.js
@@ -1,6 +1,6 @@
-import Card from './Card';
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import Card from './Card'
+import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
 
 export default function Expenses(props){
   return (
