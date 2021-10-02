9177d07a61440d97c5f76acb05f0e131a9003f22
thays <thayscasado@hotmail.com>
2021-10-02 02:35:05 -0700

    57. Adding Two-Way Binding

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,11 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+
+    // Clear the form two way binding adding the values to the state
+    setTitle("");
+    setAmount("");
+    setDate("");
   };
 
   return (
@@ -36,7 +41,7 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={title} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -44,6 +49,7 @@ const ExpenseForm = () => {
             type="Number"
             min={"0.01"}
             step={"0.01"}
+            value={amount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -53,6 +59,7 @@ const ExpenseForm = () => {
             type="date"
             min={"2021-01-01"}
             max={"2022-12-31"}
+            value={date}
             onChange={dateChangeHandler}
           />
         </div>
