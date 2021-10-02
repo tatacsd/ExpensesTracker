e09d1928499c5544a416c20031896b241c8d3859
thays <thayscasado@hotmail.com>
2021-10-02 00:57:30 -0700

    event handler

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,9 @@ import Card from "../UI/Card/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +15,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* function without parentheses, so it is a pointer only */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
