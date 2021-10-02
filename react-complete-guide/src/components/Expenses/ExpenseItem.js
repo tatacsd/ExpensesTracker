78a2f9b188dab513f2e465d53eb0beab3d9f05a2
thays <thayscasado@hotmail.com>
2021-10-02 01:24:07 -0700

    useState

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
+  // array destructuring
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("clicked");
+    setTitle("updated");
+    console.log(title);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       {/* function without parentheses, so it is a pointer only */}
