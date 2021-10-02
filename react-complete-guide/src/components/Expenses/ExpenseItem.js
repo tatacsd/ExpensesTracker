ad3f58830f13ac0bec6525247e1735bda2b3f4b4
thays <thayscasado@hotmail.com>
2021-10-02 04:02:42 -0700

    60. Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import Card from "../UI/Card/Card";
 import ExpenseDate from "./ExpenseDate";
 
+// Stateless component or dumb component
+// No state, no lifecycle methods
 const ExpenseItem = (props) => {
-  // array destructuring
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("updated");
-    console.log(title);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* function without parentheses, so it is a pointer only */}
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
 
-// to use the component outside of the file we need to export it
 export default ExpenseItem;
