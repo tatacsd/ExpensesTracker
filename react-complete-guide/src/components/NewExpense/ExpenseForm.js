224b02d55a749d9262d5b82378c6c3d03badfaf3
thays <thayscasado@hotmail.com>
2021-10-06 13:08:20 -0700

    71.Fixing a Small Bug

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: title,
-      amount: amount,
+      amount: +amount,
       date: new Date(date),
     };
 
